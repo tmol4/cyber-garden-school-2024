adb8914fd754de498ff9a950f7d90f11d17f2049
olegogonich <olegogonich240708@gmail.com>
2024-11-22 21:32:15 +0300

    dp

--- apps/backend/db.py
+++ apps/backend/db.py
@@ -4,18 +4,11 @@ conn = sqlite3.connect('db1.db')
 
 cursor = conn.cursor()
 
+cursor.execute('''INSERT INTO users (money) VALUES (35000)''')
 
-cursor.execute('''CREATE TABLE users
-             	(id INTEGER PRIMARY KEY, money INTEGER, chance_bad INTEGER, credit INTEGER)''')
-#
-# cursor.execute('''CREATE TABLE events
-#             	(id INTEGER PRIMARY KEY, description TEXT, list_answers_id TEXT )''')
-#
-#
-# cursor.execute('''CREATE TABLE answers
-#             	(id INTEGER PRIMARY KEY, answ_text TEXT, list_events_id  TEXT, minus_money INTEGER )''')
+data = cursor.execute('''SELECT * FROM users''')
 
-print(cursor.execute('''SELECT * FROM users''').fetchall())
+print(data.fetchall())
 
 conn.commit()
 conn.close()
