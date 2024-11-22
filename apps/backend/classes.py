9e608a94a71bc161182dbf0a34a9dbff86ac7db8
olegogonich <olegogonich240708@gmail.com>
2024-11-22 22:32:46 +0300

    dp

---
+++ apps/backend/classes.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, data):
+        self.id, self.money, self.chance_bad, self.chosen_answers = data
+
+class Event:
+    pass
+
+
--- apps/backend/db.py
+++ apps/backend/db.py
@@ -1,14 +1,41 @@
 import sqlite3
+from classes import User
 
-conn = sqlite3.connect('db1.db')
 
-cursor = conn.cursor()
+class DB:
+    conn = None
+    cursor = None
 
-cursor.execute('''INSERT INTO users (money) VALUES (35000)''')
 
-data = cursor.execute('''SELECT * FROM users''')
+def connect_db():
+    DB.conn = sqlite3.connect('db.db')
+    DB.cursor = DB.conn.cursor()
 
-print(data.fetchall())
 
-conn.commit()
-conn.close()
+def close_dp():
+    DB.conn.close()
+
+# cursor.execute('''CREATE TABLE users
+#                (id INTEGER PRIMARY KEY, money INTEGER, chance_bad INTEGER, list_chosen_answers INTEGER)''')
+#
+# cursor.execute('''CREATE TABLE events
+#               (id INTEGER PRIMARY KEY, description TEXT, list_answers_id TEXT )''')
+#
+# cursor.execute('''CREATE TABLE answers
+#               (id INTEGER PRIMARY KEY, answ_text TEXT, list_events_id  TEXT, minus_money INTEGER )''')
+
+
+# cursor.execute('''INSERT INTO users (money) VALUES (35000)''')
+
+# data = cursor.execute('''SELECT * FROM users''')
+#
+# print(data.fetchall())
+#
+# conn.commit()
+# conn.close()
+
+
+def get_user(_id: int) -> User:
+    user = DB.cursor.execute(f'''SELECT * FROM users WHERE id={_id}''')
+    return User(user.fetchone())
+
