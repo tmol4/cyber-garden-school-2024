e958c7536217b4ab02e5884b279608fc62c75c01
DeMineArchiver <minearchiver@gmail.com>
2024-11-23 22:07:10 +0300

    Remove auth routes

--- apps/frontend/src/routes/(auth)/_auth.register.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-function RouteComponent() {
-  return "Hello /_auth/register!";
-}
-
-export const Route = createLazyFileRoute("/(auth)/_auth/register")({
-  component: RouteComponent,
-});
