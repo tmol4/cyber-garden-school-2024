f7b55937054c321d5a40d0dd65fb27c839b8d298
deminearchiver <minearchiver@gmail.com>
2024-11-24 10:50:34 +0300

    Refactoring

--- apps/frontend/src/utils/api.ts
+++ apps/frontend/src/utils/api.ts
@@ -41,3 +41,9 @@ export type APIUser = {
 }
 
 export type APISession = APIIntroSession | APIEventSession | APIEndingSession;
+
+
+
+export const API_ENDPOINT = import.meta.env.DEV
+  ? new URL("http://localhost:5000")
+  : new URL("http://93.183.80.138:8080")
